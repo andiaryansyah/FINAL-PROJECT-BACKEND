533f4759eeb0ed80a1beb94021d6aa58f70e93c2
andiaryansyah <aariansyah70@gmail.com>
2022-08-25 16:59:20 +0800

    update files

--- storage/definers.js
+++ storage/definers.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const { applyRelation } = require('./relation');
 
 //check
-const db = new Sequelize('sql6515003','sql6515003','f8psr6rv64','3306',{
+const db = new Sequelize('sql6515003','sql6515003','f8psr6rv64',{
     host:"sql6.freesqldatabase.com",
     dialect:"mysql"
 });
